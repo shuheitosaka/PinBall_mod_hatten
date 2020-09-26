bae6f204280702761d2c3dede91942649b6256ae
shuheitosaka <70055119+shuheitosaka@users.noreply.github.com>
2020-09-26 18:30:21 +0900

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class BallController : MonoBehaviour
+{
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+    private GameObject ScoreText;
+    private int score = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.ScoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合の処理
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)//ボールが障害物に衝突した場合に得点を加算し表示する。障害物はタグで区別する。
+    {
+        if (collision.gameObject.name == "SmallStarPrefab")
+        {
+            score += 10;
+            this.ScoreText.GetComponent<Text>().text =
+                "現在の得点は" + score.ToString("F5");
+        }
+        else if (collision.gameObject.name == "SmallStar")
+        {
+            score += 10;
+            this.ScoreText.GetComponent<Text>().text =
+                "現在の得点は" + score.ToString("F5");
+        }
+        else if (collision.gameObject.name == "LargeCloud")
+        {
+            score += 20;
+            this.ScoreText.GetComponent<Text>().text =
+                "現在の得点は" + score.ToString("F5");
+        }
+        else if (collision.gameObject.name == "SmallCloudPrefab")
+        {
+            score += 30;
+            this.ScoreText.GetComponent<Text>().text =
+                "現在の得点は" + score.ToString("F5");
+        }
+        else if (collision.gameObject.name == "SmallCloud")
+        {
+            score += 30;
+            this.ScoreText.GetComponent<Text>().text =
+                "現在の得点は" + score.ToString("F5");
+        }
+        else if (collision.gameObject.name == "LargeStarPrefab")
+        {
+            score += 1000;
+            this.ScoreText.GetComponent<Text>().text =
+                "現在の得点は" + score.ToString("F5");
+        }
+        else if (collision.gameObject.name == "LargeStar")
+        {
+            score += 1000;
+            this.ScoreText.GetComponent<Text>().text =
+                "現在の得点は" + score.ToString("F5");
+        }
+
+    }
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,94 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class FripperController : MonoBehaviour
+{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Use this for initialization
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         //スマホ対応
+        for (int i = 0; i < Input.touchCount; i++)//画面がタッチされていた場合
+        {
+            Touch touch = Input.GetTouch(i);//i番目にタッチされている指の情報を取得する。
+
+            if (tag == "LeftFripperTag")//LeftFripperTagに関して
+            {
+                if (touch.position.x <= Screen.width / 2)//座標が画面の左側で
+                {
+                    if (touch.phase == TouchPhase.Began)//画面がタッチされた瞬間に
+                    {
+                        SetAngle(this.flickAngle);//LeftFripperをFlickAngleにする
+                    }
+                    else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)//画面から指が離れた瞬間に
+                    {
+                        SetAngle(this.defaultAngle); //LeftFripperをDefaultAngleにする
+                    }
+                }
+            }
+
+            if (tag == "RightFripperTag")//RightFripperTagに関して
+            {
+                if (touch.position.x >= Screen.width / 2)//座標が画面の右側で
+                {
+                    if (touch.phase == TouchPhase.Began)//画面がタッチされた瞬間に
+                    {
+                        SetAngle(this.flickAngle);//LeftFripperをFlickAngleにする
+                    }
+                    else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)//画面から指が離れた瞬間に
+                    {
+                        SetAngle(this.defaultAngle);//LeftFripperをDefaultAngleにする
+                    }
+                }
+            }
+
+        }
+
+        //PC対応
+        if (Input.GetMouseButtonDown(0) && Input.mousePosition.x <= Screen.width / 2 && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        if (Input.GetMouseButtonDown(0) && Input.mousePosition.x >= Screen.width / 2 && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        if (Input.GetMouseButtonUp(0) && Input.mousePosition.x <= Screen.width / 2 && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+        if (Input.GetMouseButtonUp(0) && Input.mousePosition.x >= Screen.width / 2 && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
